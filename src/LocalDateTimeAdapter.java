5ee312a880b91cd07ad85da6add9188537e2244f
Ekaterina Sorokina <ekaterina.sorokina@ural.rt.ru>
2025-07-17 10:43:25 +0500

    sprint_9-solution-http-api (fix): ошибка стилей

--- src/LocalDateTimeAdapter.java
+++ src/LocalDateTimeAdapter.java
@@ -7,15 +7,15 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
-    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
 
     @Override
     public void write(final JsonWriter jsonWriter, final LocalDateTime localDateTime) throws IOException {
-        jsonWriter.value(localDateTime.format(dtf));
+        jsonWriter.value(localDateTime.format(dateTimeFormatter));
     }
 
     @Override
     public LocalDateTime read(final JsonReader jsonReader) throws IOException {
-        return LocalDateTime.parse(jsonReader.nextString(), dtf);
+        return LocalDateTime.parse(jsonReader.nextString(), dateTimeFormatter);
     }
-} 
+}
